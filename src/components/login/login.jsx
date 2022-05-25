760931bac2ea3cb8ae80375e212d45601a61f80d
HSH <seongho@HSHui-Macmini.local>
2022-05-25 19:47:14 +0900

    test

--- src/components/login/login.jsx
+++ src/components/login/login.jsx
@@ -1,14 +1,32 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import Header from "../header/header";
 import Footer from "../footer/footer";
 import styles from "./login.module.css";
+import {useHistory} from "react-router-dom";
 
 const Login = ({authService}) => {
-    const onLogin = (event) => {
+    const history = useHistory(); // 다른 라우터로 갈 때는 history를 이용하면 된다.
+
+    const goToMaker = userId => {
+        history.push({
+            pathname: '/maker',
+            state: {id: userId},
+        });
+    };
+
+    const onLogin = event => {
         authService //
             .login(event.currentTarget.textContent) //
-            .then(console.log);
+            .then(data => goToMaker(data.user.uid)); // 로그인 된 데이터가 받아지면 goToMaker에 uid를 넘겨준다.
     };
+
+    useEffect(() => {
+        authService
+            .onAuthChange(user => {
+               user && goToMaker(user.uid);
+            });
+    });
+
     return (
         <section className={styles.login}>
             <Header/>
